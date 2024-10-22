7a47e7011c60c429d982f6d2a6cfa644d12aae95
Dallas Palumbo <palumbodallas@gmail.com>
2024-10-22 11:46:24 -0400

    new set of announcments plus astro major version update

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,25 +2,29 @@ const Announcement = ({ visable }: { visable: boolean }) => {
 	return (
 		<>
 			<div
-				className={`announcment-container ${
-					visable ? "show" : undefined
-				}`}
+				className={`announcment-container ${visable ? "show" : undefined
+					}`}
 			>
 				<h4>Announcements</h4>
-					
-				
-        <a className="announcment" style={{fontWeight:"700"}} href="\assets\notices\Town of Triangle of Notice meeting.pdf" download>
-						Notice Of Public Hearing 09/12/24
-					</a>
-        <a className="announcment" style={{fontWeight:"700"}} href="\assets\notices\NTB Roof.pdf" download>
-					Notice To Bidders - Roof Replacement of the Triangle New York Town Hall
-					</a>
-        <a className="announcment" style={{fontWeight:"700"}} href="\assets\notices\NTB Door Openers.pdf" download>
-					Notice To Bidders - AUTOMATIC GARAGE DOOR OPENERS
-					</a>
-        <a className="announcment" style={{fontWeight:"700"}} href="\assets\notices\Snow and Ice Policy .pdf.pdf" download>
+
+
+				<a className="announcment" style={{ fontWeight: "700" }} href="\assets\notices\Ambulance legal 2024.pdf" download>
+					Notice of Public Hearing
+					Ambulance Service Contract for 2025
+				</a>
+				<a className="announcment" style={{ fontWeight: "700" }} href="\assets\notices\LEGAL NOTICE-prelim budget.pdf" download>
+					Notice of Public Hearing on the Preliminary Budget of the Town of Triangle for the Year 2025
+				</a>
+				<a className="announcment" style={{ fontWeight: "700" }} href="\assets\notices\WP FD  legal 2024.pdf" download>
+					"Notice of Public Hearing - Town of Triangle Fire District Contract with Whitney Point (Nov 14, 2024)"
+				</a>
+				<a className="announcment" style={{ fontWeight: "700" }} href="\assets\notices\TFD PH 2024" download>
+					"Notice of Public Hearing - Town of Triangle Fire District Contract with Whitney Point TFD (Nov 14, 2024)"
+				</a>
+
+				<a className="announcment" style={{ fontWeight: "700" }} href="\assets\notices\Snow and Ice Policy .pdf.pdf" download>
 					Snow And Ice Policy
-					</a>
+				</a>
 			</div>
 		</>
 	);
